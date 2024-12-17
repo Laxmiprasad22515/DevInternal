c3eb7c571d3433b12609d164b75a8a69050e58ff
Laxmiprasad <laxmiprasad22515@gmail.com>
2024-12-17 15:13:28 +0530

    Pavan

--- Fib.java
+++ Fib.java
@@ -2,19 +2,7 @@ public class Fib
 {
     public static void main(String[] args) 
     {
-        int n = 10;
-        int fTerm = 0, sTerm = 1;
-        
-        System.out.println("Fibonacci Series till " + n + " terms:");
-        
-        for (int i = 1; i <= n; ++i) 
-	{
-            System.out.print(fTerm + ", ");
-            
-            int nextTerm = fTerm + sTerm;
-            fTerm = sTerm;
-            sTerm = nextTerm;
-        }
+        System.out.println("Pavan");
     }
 }
 
